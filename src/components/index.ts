bc2d94f5aac86fc847fd0874a5ba0f9bf504cb7e
user-rq <roman.varkuta@gmail.com>
2019-11-18 18:12:18 +0200

    Fix lint and add "lint" script to package json

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,12 @@
 import AbstractFigure from "./AbstractFigure";
 import Circle from "./Circle";
+import ColorExtractor from "./ColorExtractor";
 import Dispatcher from "./Dispatcher";
 import * as Listener from "./Listener";
 import Point from "./Point";
 import Slider from "./Slider";
 import Space from "./Space";
 import Visualizer from "./Visualizer";
-import ColorExtractor from "./ColorExtractor";
 
 export {
     Circle,
